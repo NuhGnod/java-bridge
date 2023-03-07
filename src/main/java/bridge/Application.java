d04d58f7fd44d9b248ab05bb64d55173546ce914
kimdonghun <zhfptm12@naver.com>
2023-03-07 15:29:45 +0900

    Feat : main함수 시작

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        GameController gameController = new GameController(InputView.getInstance(), OutputView.getInstance());
+        gameController.startGame();
     }
+
 }
